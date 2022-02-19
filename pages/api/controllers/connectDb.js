828db2f17d9dda238f9208ad686ca4a801ee4c2d
Hirdeshgupta <hirdeshgupta16@gmail.com>
2022-02-20 01:41:48 +0530

    folder structure added

--- pages/api/controllers/connectDb.js
+++ pages/api/controllers/connectDb.js
@@ -1,6 +1,2 @@
 const mongoose = require("mongoose");
-export default async function connectDb() {
-  await mongoose.connect(process.env.MONGODB_URI, () => {
-    console.log("Mongo Db Connected...");
-  });
-}
+export default async function connectDb() {}
